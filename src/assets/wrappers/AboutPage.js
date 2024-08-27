5ba635d456f0c99c41a6b6271ec1ca29551343d9
Jensen Kershey Aragon <jnsnaragon01@gmail.com>
2024-08-28 01:13:06 +0800

    first commit

---
+++ src/assets/wrappers/AboutPage.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  h3 {
+    text-align: center;
+    margin-top: 5rem;
+  }
+
+  p {
+    line-height: 2;
+    color: var(--grey-500);
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 2rem;
+  }
+`;
+
+export default Wrapper;
